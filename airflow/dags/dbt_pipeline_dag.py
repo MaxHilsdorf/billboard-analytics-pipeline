9e8367dfffded174fbcf9dbd0ec5d027c5de695a
MaxHilsdorf <maxhilsdorf@web.de>
2024-11-02 20:56:12 +0100

    [Add] Airflow DAG now runs dbt test before dbt run

--- airflow/dags/dbt_pipeline_dag.py
+++ airflow/dags/dbt_pipeline_dag.py
@@ -1,6 +1,5 @@
 from airflow import DAG
 from datetime import datetime
-import json
 from airflow.operators.dummy import DummyOperator
 from airflow.operators.bash import BashOperator
 
@@ -10,13 +9,19 @@ with DAG(
     schedule_interval="@daily",
     catchup=False,
 ) as dag:
-    
+
     start = DummyOperator(task_id="start")
+
+    # Add a new task for dbt test
+    dbt_test = BashOperator(
+        task_id="dbt_test",
+        bash_command="cd /usr/local/airflow/dbt && dbt test",
+    )
 
     dbt_run = BashOperator(
         task_id="dbt_run",
         bash_command="cd /usr/local/airflow/dbt && dbt run",
-
     )
 
-    start >> dbt_run
+    # Set up the task dependencies
+    start >> dbt_test >> dbt_run
